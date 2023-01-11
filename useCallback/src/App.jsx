674abdde41d4eb4f6643ade64ed0ead5edd06b75
Alerrando <alerrando2@gmail.com>
2023-01-10 23:59:35 -0300

    add form and folder componente Product List

--- useCallback/src/App.jsx
+++ useCallback/src/App.jsx
@@ -3,32 +3,29 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [product, setProduct] = useState('');
+  const [productList, setProductList] = useState([]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <form onSubmit={e => handleSubmit(e)}>
+
+      </form>
     </div>
   )
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    setProductList([...productList, product]);
+    setProduct('');
+  }
+
+  function listModifier(list){
+    const spacedList = list.map(list => list.split('').join(''));
+
+    return spacedList;
+  }
 }
 
 export default App
